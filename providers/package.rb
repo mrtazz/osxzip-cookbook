1518d28f443e253f3f500dc112626068c5f82b10
Daniel Schauenberg <d@unwiredcouch.com>
2011-08-01 19:08:48 +0200

    add basic provider definition

---
+++ providers/package.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: osx_zip
+# Resource:: package
+#
+# Copyright 2011, Daniel Schauenberg
+#
+
+def load_current_resource
+  @zippkg = Chef::Resource::OsxZipPackage.new(new_resource.name)
+  @zippkg.app(new_resource.app)
+  @zippkg.source(new_resource.source)
+  @zippkg.checksum(new_resource.checksum)
+  @zippkg.destination(new_resource.destination)
+  Chef::Log.debug("Checking for application #{@zippkg.app}")
+  installed = ::File.directory?("#{@zippkg.destination}/#{@zippkg.app}.app")
+  @zippkg.installed(installed)
+end
+
+action :install do
+  unless @zippkg.installed
+  remote_file "#{Chef::Config[:file_cache_path]}/#{@zippkg.app}.zip" do
+    source @zippkg.source
+    checksum @zippkg.checksum
+  end
+
+  execute "untar #{@zippkg.app}.zip" do
+    command "unzip #{Chef::Config[:file_cache_path]}/#{@zippkg.app}.zip"
+    cwd @zippkg.destination
+    not_if @zippkg.installed
+  end
+
+  directory "#{@zippkg.destination}/__MACOSX" do
+    action :delete
+  end
+
+  execute "cleanup OSX artefacts" do
+    cwd @zippkg.destination
+    only_if ::File.directory? ""
+  end
+
+end
+
+end
