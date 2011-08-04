cc837cfcdd36c7388ab55737781282b4f36d691b
Daniel Schauenberg <d@unwiredcouch.com>
2011-08-04 06:32:02 +0200

    wrap in unless block correctly

--- providers/package.rb
+++ providers/package.rb
@@ -17,29 +17,27 @@ def load_current_resource
 end
 
 action :install do
-  unless @zippkg.installed
-  remote_file "#{Chef::Config[:file_cache_path]}/#{@zippkg.app}.zip" do
-    source @zippkg.source
-    checksum @zippkg.checksum
+  unless @zippkg.installed do
+    remote_file "#{Chef::Config[:file_cache_path]}/#{@zippkg.app}.zip" do
+      source @zippkg.source
+      checksum @zippkg.checksum
+    end
+
+    execute "unzip #{@zippkg.app}.zip" do
+      command "unzip #{Chef::Config[:file_cache_path]}/#{@zippkg.app}.zip"
+      cwd @zippkg.destination
+      not_if @zippkg.installed
+    end
+
+    file "#{@zippkg.destination}/#{@zippkg.app}.app/Contents/MacOS/#{@zippkg.app}" do
+      mode 0755
+      ignore_failure true
+    end
+
+    directory "#{@zippkg.destination}/__MACOSX" do
+      recursive true
+      action :delete
+      ignore_failure true
+    end
   end
-
-  execute "untar #{@zippkg.app}.zip" do
-    command "unzip #{Chef::Config[:file_cache_path]}/#{@zippkg.app}.zip"
-    cwd @zippkg.destination
-    not_if @zippkg.installed
-  end
-
-  file "#{@zippkg.destination}/#{@zippkg.app}.app/Contents/MacOS/#{@zippkg.app}" do
-    mode 0755
-    ignore_failure true
-  end
-
-  directory "#{@zippkg.destination}/__MACOSX" do
-    recursive true
-    action :delete
-    ignore_failure true
-  end
-
-end
-
 end
