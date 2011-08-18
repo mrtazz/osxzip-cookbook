e27a819a4e5decd93d49e139e6c6bcc18d9150d4
Daniel Schauenberg <d@unwiredcouch.com>
2011-08-18 19:28:19 +0200

    minor fixes and rename to osxzip

--- providers/package.rb
+++ providers/package.rb
@@ -6,7 +6,7 @@
 #
 
 def load_current_resource
-  @zippkg = Chef::Resource::OsxZipPackage.new(new_resource.name)
+  @zippkg = Chef::Resource::OsxzipPackage.new(new_resource.name)
   @zippkg.app(new_resource.app)
   @zippkg.source(new_resource.source)
   @zippkg.checksum(new_resource.checksum)
@@ -17,27 +17,30 @@ def load_current_resource
 end
 
 action :install do
-  unless @zippkg.installed do
-    remote_file "#{Chef::Config[:file_cache_path]}/#{@zippkg.app}.zip" do
-      source @zippkg.source
-      checksum @zippkg.checksum
+
+  unless @zippkg.installed
+
+    remote_file "#{Chef::Config[:file_cache_path]}/#{new_resource.app}.zip" do
+      source new_resource.source
+      checksum new_resource.checksum
     end
 
-    execute "unzip #{@zippkg.app}.zip" do
-      command "unzip #{Chef::Config[:file_cache_path]}/#{@zippkg.app}.zip"
-      cwd @zippkg.destination
-      not_if @zippkg.installed
+    execute "unzip #{new_resource.app}.zip" do
+      command "unzip #{Chef::Config[:file_cache_path]}/#{new_resource.app}.zip"
+      cwd new_resource.destination
+      not_if new_resource.installed
     end
 
-    file "#{@zippkg.destination}/#{@zippkg.app}.app/Contents/MacOS/#{@zippkg.app}" do
+    file "#{new_resource.destination}/#{new_resource.app}.app/Contents/MacOS/#{new_resource.app}" do
       mode 0755
       ignore_failure true
     end
 
-    directory "#{@zippkg.destination}/__MACOSX" do
+    directory "#{new_resource.destination}/__MACOSX" do
       recursive true
       action :delete
       ignore_failure true
     end
+
   end
 end
